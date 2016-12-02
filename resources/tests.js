a3c91dc2b319a487c096d6546b41d218084e4b2a
Anthony Gubler <agubler@sitepen.com>
2016-12-02 07:39:54 +0000

    add react typescript

--- resources/tests.js
+++ resources/tests.js
@@ -156,11 +156,48 @@ Suites.push({
 });
 
 Suites.push({
+    name: 'React Typescript',
+    url: 'todomvc/examples/typescript-react/index.html',
+    version: '?.?.?',
+    prepare: function (runner, contentWindow, contentDocument) {
+        return runner.waitForElement('.new-todo').then(function (element) {
+            element.focus();
+            return element;
+        });
+    },
+    tests: [
+        new BenchmarkTestStep('Adding' + numberOfItemsToAdd + 'Items', function (newTodo, contentWindow, contentDocument) {
+            for (var i = 0; i < numberOfItemsToAdd; i++) {
+                var changeEvent = document.createEvent('Event');
+                changeEvent.initEvent('input', true, true);
+                newTodo.value = 'Something to do ' + i;
+                newTodo.dispatchEvent(changeEvent);
+
+                var keypressEvent = document.createEvent('Event');
+                keypressEvent.initEvent('keydown', true, true);
+                keypressEvent.keyCode = 13;
+                newTodo.dispatchEvent(keypressEvent);
+            }
+        }),
+        new BenchmarkTestStep('CompletingAllItems', function (newTodo, contentWindow, contentDocument) {
+            var checkboxes = contentDocument.querySelectorAll('.toggle');
+            for (var i = 0; i < checkboxes.length; i++)
+                checkboxes[i].click();
+        }),
+        new BenchmarkTestStep('DeletingAllItems', function (newTodo, contentWindow, contentDocument) {
+            var deleteButtons = contentDocument.querySelectorAll('.destroy');
+            for (var i = 0; i < deleteButtons.length; i++)
+                deleteButtons[i].click();
+        })
+    ]
+});
+
+Suites.push({
     name: 'React',
     url: 'todomvc/react/index.html',
     version: '0.10.0',
     prepare: function (runner, contentWindow, contentDocument) {
-        return runner.waitForElement('.new-todo').then(function (element) {
+        return runner.waitForElement('#new-todo').then(function (element) {
             element.focus();
             return element;
         });
